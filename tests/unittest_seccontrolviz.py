3105f5ef85b3de1ad97ceedf3b7cae6b1d44a91e
Greg Elin <greg@fotonotes.net>
2015-08-08 11:49:07 -0400

    Update brittle tests to match modified node and edge properties

--- tests/unittest_seccontrolviz.py
+++ tests/unittest_seccontrolviz.py
@@ -52,16 +52,17 @@ class SecControlVizTest(unittest.TestCase):
 		cv = SecControlViz(id)
 		node_options = cv.node_options_by_id(id)
 		# print "node_options: ", node_options
-		self.assertTrue(node_options == {'fontname': 'arial', 'color': 'red', 'label': u'AU-3\nCONTENT OF AUDIT RECORDS', 'shape': 'box3d', 'fontsize': '12', 'fontcolor': 'red'})
+		self.assertTrue(node_options == {'fontname': 'arial', 'URL': '/control?id=AU-3', 'tooltip': u'(AU-3) Content Of Audit Records', 'label': u'AU-3\nContent Of Audit Records', 'color': 'palevioletred', 'shape': 'egg', 'fontsize': '12', 'fontcolor': 'palevioletred'})
 
 	def test_create_node_options_tuples(self):
+		# To Do: Make shape and attributes passed in variables
 		id = "AU-3"
 		cv = SecControlViz(id)
 		# Find precursor nodes
 		cv.precursor_list(cv.dep_dict, id, cv.nodes)
 		# print "cv.nodes: ", cv.nodes
 		# print cv.node_options_tuples(cv.nodes)
-		self.assertTrue(cv.node_options_tuples(cv.nodes) == [('AU-3', {'fontname': 'arial', 'color': 'red', 'label': u'AU-3\nCONTENT OF AUDIT RECORDS', 'shape': 'box3d', 'fontsize': '12', 'fontcolor': 'red'}), ('AU-2', {'fontname': 'arial', 'color': 'blue', 'label': u'AU-2\nAUDIT EVENTS', 'shape': 'box3d', 'fontsize': '12', 'fontcolor': 'blue'}), ('RA-3', {'fontname': 'arial', 'color': 'blue', 'label': u'RA-3\nRISK ASSESSMENT', 'shape': 'box3d', 'fontsize': '12', 'fontcolor': 'blue'}), ('PM-9', {'fontname': 'arial', 'color': 'blue', 'label': u'PM-9\nRISK MANAGEMENT STRATEGY', 'shape': 'box3d', 'fontsize': '12', 'fontcolor': 'blue'})])
+		self.assertTrue(cv.node_options_tuples(cv.nodes) == [('AU-3', {'fontname': 'arial', 'URL': '/control?id=AU-3', 'tooltip': u'(AU-3) Content Of Audit Records', 'label': u'AU-3\nContent Of Audit Records', 'color': 'palevioletred', 'shape': 'egg', 'fontsize': '12', 'fontcolor': 'palevioletred'}), ('AU-2', {'fontname': 'arial', 'URL': '/control?id=AU-2', 'tooltip': u'(AU-2) Audit Events', 'label': u'AU-2\nAudit Events', 'color': 'cornflowerblue', 'shape': 'egg', 'fontsize': '12', 'fontcolor': 'cornflowerblue'}), ('RA-3', {'fontname': 'arial', 'URL': '/control?id=RA-3', 'tooltip': u'(RA-3) Risk Assessment', 'label': u'RA-3\nRisk Assessment', 'color': 'cornflowerblue', 'shape': 'egg', 'fontsize': '12', 'fontcolor': 'cornflowerblue'}), ('PM-9', {'fontname': 'arial', 'URL': '/control?id=PM-9', 'tooltip': u'(PM-9) Risk Management Strategy', 'label': u'PM-9\nRisk Management Strategy', 'color': 'cornflowerblue', 'shape': 'egg', 'fontsize': '12', 'fontcolor': 'cornflowerblue'})])
 
 	def test_edges(self):
 		id = "AU-3"
@@ -70,9 +71,10 @@ class SecControlVizTest(unittest.TestCase):
 		for node in cv.nodes:
 			cv.precursor_edges(cv.dep_dict, node, cv.edges)
 		# print "edges: ", cv.edges
-		self.assertTrue(cv.edges == [('AU-2', 'AU-3'), ('RA-3', 'AU-2'), ('PM-9', 'RA-3')])
+		self.assertTrue(cv.edges == [(('AU-2', 'AU-3'), {'color': 'darkkhaki', 'arrowhead': 'open'}), (('RA-3', 'AU-2'), {'color': 'darkkhaki', 'arrowhead': 'open'}), (('PM-9', 'RA-3'), {'color': 'darkkhaki', 'arrowhead': 'open'})])
 
 	def test_add_nodes(self):
+		# To Do: Make shape and attributes passed in variables
 		id = "AU-3"
 		cv = SecControlViz(id)
 		cv.precursor_list(cv.dep_dict, id, cv.nodes)
@@ -81,13 +83,13 @@ class SecControlVizTest(unittest.TestCase):
 		# print cv.nodes
 		self.assertTrue("%s" % digraph == """digraph {
 	"AU-3" [label="AU-3
-CONTENT OF AUDIT RECORDS" color=red fontcolor=red fontname=arial fontsize=12 shape=box3d]
+Content Of Audit Records" URL="/control?id=AU-3" color=palevioletred fontcolor=palevioletred fontname=arial fontsize=12 shape=egg tooltip="(AU-3) Content Of Audit Records"]
 	"AU-2" [label="AU-2
-AUDIT EVENTS" color=blue fontcolor=blue fontname=arial fontsize=12 shape=box3d]
+Audit Events" URL="/control?id=AU-2" color=cornflowerblue fontcolor=cornflowerblue fontname=arial fontsize=12 shape=egg tooltip="(AU-2) Audit Events"]
 	"RA-3" [label="RA-3
-RISK ASSESSMENT" color=blue fontcolor=blue fontname=arial fontsize=12 shape=box3d]
+Risk Assessment" URL="/control?id=RA-3" color=cornflowerblue fontcolor=cornflowerblue fontname=arial fontsize=12 shape=egg tooltip="(RA-3) Risk Assessment"]
 	"PM-9" [label="PM-9
-RISK MANAGEMENT STRATEGY" color=blue fontcolor=blue fontname=arial fontsize=12 shape=box3d]
+Risk Management Strategy" URL="/control?id=PM-9" color=cornflowerblue fontcolor=cornflowerblue fontname=arial fontsize=12 shape=egg tooltip="(PM-9) Risk Management Strategy"]
 }"""
 )
 
